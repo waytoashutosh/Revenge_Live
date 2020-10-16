e9d1353e0031715a4b4f38b53907a4c83c0038ed
Ashutosh Kumar <ashutoshk.4300@gmail.com>
2020-10-17 02:16:37 +0530

    Video Input done Now need to send the video then think about compression.

--- src/Begin/login_signup_Controller.java
+++ src/Begin/login_signup_Controller.java
@@ -6,12 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import org.opencv.core.Core;
 
 import java.io.IOException;
 
 public class login_signup_Controller {
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
     public Button log_in;
     public Button sign_up;
+    public Button test_video;
 
     public void log_in_listener(ActionEvent actionEvent) {
         Stage stage = (Stage) log_in.getScene().getWindow();
@@ -29,4 +32,16 @@ public class login_signup_Controller {
 
 
     }
+
+    public void test_video_listener(ActionEvent actionEvent) {
+        Stage stage = (Stage) test_video.getScene().getWindow();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("testing_video_scene.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(new Scene(root,700,500));
+    }
+
 }
