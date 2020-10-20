ce184ccaf18968f18ba40b7c2964f0302f13d35d
adarsh221156 <adarsh221156@gmail.com>
2020-10-20 12:20:06 +0530

    NULLpointerEXCEP ERROR RESOLVED & Database Infra changed

--- src/Begin/LogInScene_Controller.java
+++ src/Begin/LogInScene_Controller.java
@@ -9,33 +9,51 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class LogInScene_Controller {
+    Connection con = null;
+    PreparedStatement pst = null;
 
     public PasswordField password_id;
     public TextField user_id;
     public Button log_in_id;
     public Button back_id;
 
-    private String entered_pass;
-    private String user_name;
-
-    public void password_listener(ActionEvent actionEvent) {
-        entered_pass=password_id.getText();
-
-    }
-
-    public void user_id_listener(ActionEvent actionEvent) {
-        user_name = user_id.getText();
-
-
-    }
+    public String entered_pass="",forconfirmation="";
+    public String user_name="";
 
     public void log_in_listener(ActionEvent actionEvent) {
-        //we nee to check if the user name exists in the database
-        //if the username exists then match password
-        //if matches then login to dashboard
+        try{
+            entered_pass=password_id.getText();
+            user_name = user_id.getText();
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/revenge_live","root","");
+            String query = "SELECT * FROM user_info WHERE userid='"+user_name+"'";
+            pst=con.prepareStatement(query);
+            ResultSet res = pst.executeQuery();
+
+            if(res.next()){
+                forconfirmation=res.getString("password");
+                if(forconfirmation.equals(entered_pass)){
+                    JOptionPane.showMessageDialog(null, "WELCOME ");
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "INCORRECT PASSWORD.");
+                }
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "USER DOESN'T EXISTS.");
+            }
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
     }
 
     public void back_listener(ActionEvent actionEvent) {
